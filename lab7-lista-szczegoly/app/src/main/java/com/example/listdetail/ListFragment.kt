8e7e3ce7951c3024185f94f0da4d7240dbdcb036
Endrju00 <andrzej.kapczynski21@gmail.com>
2022-05-23 13:40:37 +0200

    Set fragment in MainActivity

---
+++ lab7-lista-szczegoly/app/src/main/java/com/example/listdetail/ListFragment.kt
@@ -0,0 +1,10 @@
+package com.example.listdetail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ListFragment : Fragment(R.layout.fragment_list)
--- lab7-lista-szczegoly/app/src/main/java/com/example/listdetail/MainActivity.kt
+++ lab7-lista-szczegoly/app/src/main/java/com/example/listdetail/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.listdetail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<ListFragment>(R.id.list_fragment)
+            }
+        }
     }
 }
