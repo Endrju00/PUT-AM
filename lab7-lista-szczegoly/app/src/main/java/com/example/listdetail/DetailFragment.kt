735556f2ba9987f63204450d6d107be2d8346e8d
Endrju00 <andrzej.kapczynski21@gmail.com>
2022-05-24 00:47:14 +0200

    Add routes details display

--- lab7-lista-szczegoly/app/src/main/java/com/example/listdetail/DetailFragment.kt
+++ lab7-lista-szczegoly/app/src/main/java/com/example/listdetail/DetailFragment.kt
@@ -10,10 +10,11 @@ import androidx.fragment.app.Fragment
 
 class DetailFragment : Fragment(R.layout.fragment_detail) {
     companion object {
-        fun newInstance(element: String): DetailFragment {
+        fun newInstance(route: String, description: String): DetailFragment {
             val fragment = DetailFragment()
             val args = Bundle()
-            args.putString("element", element)
+            args.putString("route", route)
+            args.putString("description", description)
             fragment.arguments = args
             return fragment
         }
@@ -25,6 +26,8 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val textView = view.findViewById<TextView>(R.id.detail_textview)
-        textView.text = this.arguments?.getString("element")
+        val textView2 = view.findViewById<TextView>(R.id.detail_textview2)
+        textView.text = this.arguments?.getString("route")
+        textView2.text = this.arguments?.getString("description")
     }
 }
--- lab7-lista-szczegoly/app/src/main/java/com/example/listdetail/ListFragment.kt
+++ lab7-lista-szczegoly/app/src/main/java/com/example/listdetail/ListFragment.kt
@@ -19,35 +19,30 @@ class ListFragment : Fragment(R.layout.fragment_list) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val lv = view.findViewById<ListView>(R.id.listview)
-        val list = arrayOf(
-            "Droga 1",
-            "Droga 2",
-            "Droga 3",
-            "Droga 4",
-            "Droga 5",
-            "Droga 6",
-            "Droga 7",
-            "Droga 8",
-            "Droga 9",
-            "Droga 10",
-            "Droga 11",
-            "Droga 12",
-            "Droga 13",
-            "Droga 14",
-            "Droga 15",
-            "Droga 16",
-            "Droga 17",
-            "Droga 18",
-            "Droga 19",
-            "Droga 20",
-            )
+        val routes = arrayOf(
+            "Droga 1", "Droga 2", "Droga 3", "Droga 4", "Droga 5",
+            "Droga 6", "Droga 7", "Droga 8", "Droga 9", "Droga 10",
+            "Droga 11", "Droga 12", "Droga 13", "Droga 14", "Droga 15",
+            "Droga 16", "Droga 17", "Droga 18", "Droga 19", "Droga 20",
+        )
+        val descriptions = mapOf(
+            "Droga 1" to "Opis drogi 1", "Droga 2" to "Opis drogi 2", "Droga 3" to "Opis drogi 3",
+            "Droga 4" to "Opis drogi 4", "Droga 5" to "Opis drogi 5", "Droga 6" to "Opis drogi 6",
+            "Droga 7" to "Opis drogi 7", "Droga 8" to "Opis drogi 8", "Droga 9" to "Opis drogi 9",
+            "Droga 10" to "Opis drogi 10", "Droga 11" to "Opis drogi 11", "Droga 12" to "Opis drogi 12",
+            "Droga 13" to "Opis drogi 13", "Droga 14" to "Opis drogi 14", "Droga 15" to "Opis drogi 15",
+            "Droga 16" to "Opis drogi 16", "Droga 17" to "Opis drogi 17", "Droga 18" to "Opis drogi 18",
+            "Droga 19" to "Opis drogi 19", "Droga 20" to "Opis drogi 20",
+        )
 
-        val adapter = ArrayAdapter(view.context, android.R.layout.simple_list_item_1, list)
+
+        val adapter = ArrayAdapter(view.context, android.R.layout.simple_list_item_1, routes)
         lv.adapter = adapter
 
         lv.setOnItemClickListener { parent, view, position, id ->
-            val element = adapter.getItem(position)
-            replaceFragment(DetailFragment.newInstance(element.toString()))
+            val route = adapter.getItem(position)
+            val description = descriptions[route]
+            replaceFragment(DetailFragment.newInstance(route.toString(), description.toString()))
         }
     }
 
