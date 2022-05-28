c8caba341f2fd922e0f43c2ed0d62d58fca354df
Endrju00 <andrzej.kapczynski21@gmail.com>
2022-05-28 21:07:29 +0200

    Add dark mode

--- lab10-11/app/src/main/java/com/example/listdetail/InfoFragment.kt
+++ lab10-11/app/src/main/java/com/example/listdetail/InfoFragment.kt
@@ -5,5 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.lang.Float
 
-class InfoFragment : Fragment(R.layout.fragment_info)
+class InfoFragment : Fragment(R.layout.fragment_info) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val switch = view.findViewById<Switch>(R.id.switch1)
+
+        val sharedTime = requireActivity().getSharedPreferences("com.example.listdetail.shared",0)
+        val darkMode = sharedTime.getBoolean("dark_mode", false)
+
+        if (darkMode)
+            AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+
+        switch.isChecked = darkMode
+
+        val edit = sharedTime.edit()
+        switch.setOnCheckedChangeListener { _, checked ->
+            if (checked) {
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+                edit.putBoolean("dark_mode", true)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+                edit.putBoolean("dark_mode", false)
+            }
+            edit.apply()
+        }
+    }
+}
