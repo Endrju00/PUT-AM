f4e36228168bc4431181604bc2118ca78d8f0c7b
Endrju00 <andrzej.kapczynski21@gmail.com>
2022-05-28 17:50:10 +0200

    Add RecyclerView

--- lab10-11/app/src/main/java/com/example/listdetail/ListFragment.kt
+++ lab10-11/app/src/main/java/com/example/listdetail/ListFragment.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.ListView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 class ListFragment : Fragment(R.layout.fragment_list) {
+
     companion object {
         fun newInstance(difficulty: String): ListFragment {
             val fragment = ListFragment()
@@ -25,23 +26,12 @@ class ListFragment : Fragment(R.layout.fragment_list) {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val lv = view.findViewById<ListView>(R.id.listview)
         val difficulty = this.arguments?.getString("difficulty")
-
         var routes = arrayOf(
             "Droga 1", "Droga 2", "Droga 3", "Droga 4", "Droga 5",
             "Droga 6", "Droga 7", "Droga 8", "Droga 9", "Droga 10",
             "Droga 11", "Droga 12", "Droga 13", "Droga 14", "Droga 15",
             "Droga 16", "Droga 17", "Droga 18", "Droga 19", "Droga 20",
-        )
-        val descriptions = mapOf(
-            "Droga 1" to "Opis drogi 1", "Droga 2" to "Opis drogi 2", "Droga 3" to "Opis drogi 3",
-            "Droga 4" to "Opis drogi 4", "Droga 5" to "Opis drogi 5", "Droga 6" to "Opis drogi 6",
-            "Droga 7" to "Opis drogi 7", "Droga 8" to "Opis drogi 8", "Droga 9" to "Opis drogi 9",
-            "Droga 10" to "Opis drogi 10", "Droga 11" to "Opis drogi 11", "Droga 12" to "Opis drogi 12",
-            "Droga 13" to "Opis drogi 13", "Droga 14" to "Opis drogi 14", "Droga 15" to "Opis drogi 15",
-            "Droga 16" to "Opis drogi 16", "Droga 17" to "Opis drogi 17", "Droga 18" to "Opis drogi 18",
-            "Droga 19" to "Opis drogi 19", "Droga 20" to "Opis drogi 20",
         )
 
         when (difficulty) {
@@ -49,14 +39,9 @@ class ListFragment : Fragment(R.layout.fragment_list) {
             "difficult" -> routes = getDifficultRoutes(routes)
         }
 
-        val adapter = ArrayAdapter(view.context, android.R.layout.simple_list_item_1, routes)
-        lv.adapter = adapter
-
-        lv.setOnItemClickListener { _, _, position, _ ->
-            val route = adapter.getItem(position)
-            val description = descriptions[route]
-            replaceFragment(DetailFragment.newInstance(route.toString(), description.toString()))
-        }
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerview)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = RecyclerAdapter(routes)
     }
 
     private fun getDifficultRoutes(routes: Array<String>): Array<String> {
@@ -69,17 +54,5 @@ class ListFragment : Fragment(R.layout.fragment_list) {
         val list = mutableListOf<String>()
         routes.forEachIndexed{ i, v -> if (i%2==1) list.add(v)}
         return list.toTypedArray()
-    }
-
-    private fun replaceFragment(fragment: Fragment) {
-        val smallestWidth = resources.configuration.smallestScreenWidthDp
-        val fragmentTransaction = parentFragmentManager.beginTransaction()
-
-        if (smallestWidth < 720)
-            fragmentTransaction.replace(R.id.fragment_container, fragment)
-        else
-            fragmentTransaction.replace(R.id.fragment_container2, fragment)
-
-        fragmentTransaction.commit()
     }
 }
---
+++ lab10-11/app/src/main/java/com/example/listdetail/RecyclerAdapter.kt
@@ -0,0 +1,67 @@
+package com.example.listdetail
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapter(private val routes: Array<String>): RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+    private val descriptions = mapOf(
+        "Droga 1" to "Opis drogi 1", "Droga 2" to "Opis drogi 2", "Droga 3" to "Opis drogi 3",
+        "Droga 4" to "Opis drogi 4", "Droga 5" to "Opis drogi 5", "Droga 6" to "Opis drogi 6",
+        "Droga 7" to "Opis drogi 7", "Droga 8" to "Opis drogi 8", "Droga 9" to "Opis drogi 9",
+        "Droga 10" to "Opis drogi 10", "Droga 11" to "Opis drogi 11", "Droga 12" to "Opis drogi 12",
+        "Droga 13" to "Opis drogi 13", "Droga 14" to "Opis drogi 14", "Droga 15" to "Opis drogi 15",
+        "Droga 16" to "Opis drogi 16", "Droga 17" to "Opis drogi 17", "Droga 18" to "Opis drogi 18",
+        "Droga 19" to "Opis drogi 19", "Droga 20" to "Opis drogi 20",
+    )
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.card_layout, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerAdapter.ViewHolder, position: Int) {
+        holder.itemTitle.text = routes[position]
+        holder.itemImage.setImageResource(R.drawable.route)
+    }
+
+    override fun getItemCount(): Int {
+        return routes.size
+    }
+
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var itemImage: ImageView
+        var itemTitle: TextView
+
+        init {
+            itemImage = itemView.findViewById(R.id.item_image)
+            itemTitle = itemView.findViewById(R.id.item_title)
+
+            itemView.setOnClickListener {
+                val position: Int = adapterPosition
+                val route = routes[position]
+                val description = descriptions[route]
+                replaceFragment(DetailFragment.newInstance(route, description.toString()))
+            }
+        }
+
+        private fun replaceFragment(fragment: Fragment) {
+            val smallestWidth = itemView.resources.configuration.smallestScreenWidthDp
+            val manager = (itemView.context as FragmentActivity).supportFragmentManager
+            val fragmentTransaction = manager.beginTransaction()
+
+            if (smallestWidth < 720)
+                fragmentTransaction.replace(R.id.fragment_container, fragment)
+            else
+                fragmentTransaction.replace(R.id.fragment_container2, fragment)
+
+            fragmentTransaction.commit()
+        }
+    }
+}
